e4fc557de85903df239330f563cc94ae2d4c8a99
Yael-TEB <128626946+Yael-TEB@users.noreply.github.com>
2023-04-14 11:57:17 -0600

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,16 @@
+
+def leer_lab():
+  with open("mapa2.txt", "r") as f:
+    lineas = f.readlines()
+  lab = []  #lista para guardar la matriz
+  for linea in lineas:
+    fila = [int(num) for num in linea.strip().split(",")]
+    lab.append(fila)
+  return lab
+  
+matriz = leer_lab()
+
+print("Matriz leida: ")
+for fila in matriz:
+  print(fila)
+ 
